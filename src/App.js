0a05ff8a5d17558c254437ffc1f63aa8997c831f
unknown <yashsahay999@gmail.com>
2022-08-15 22:18:56 +0530

    Added basic structure and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: "What is React?",
+        content: "React is a Javascript fron-end library. It is used to show content to users and handle user events. Enginners use React to create reusable components."
+    },
+    {
+        title: "How do we show content with React?",
+        content: "We show content in React with the help of JSX."
+    },
+    {
+        title: "Why is React useful for enginners?",
+        content: "React is useful for engineer, because it allows them to create reusable components and structured code."
+    }
+];
 
 const App = () => {
     return (
-        <div>Widget App!</div>
+        <div>
+            <Accordion items={items} />
+        </div>
     );
 }
 
---
+++ src/components/Accordion.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Accordion = ({items}) => {
+
+    const renderedItems = items.map(({title,content}) => 
+        <React.Fragment key={title}>
+            <div className="title active">
+                <i className="dropdown icon"></i>
+                {title}
+            </div>
+            <div className="content active">
+                <p>{content}</p>
+            </div>
+        </React.Fragment>
+    )
+
+    return (
+        <div className="ui styled accordion"> 
+            {renderedItems}
+        </div>
+    );
+}
+
+export default Accordion;
