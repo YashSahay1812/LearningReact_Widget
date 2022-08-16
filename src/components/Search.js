368a6da40e9f8db8c3b076d7dafefc4745086609
unknown <yashsahay999@gmail.com>
2022-08-16 15:05:54 +0530

    Successfully making api request through Axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 const Search = () => {
+
+    const [term,setTerm] = useState("");
+
+    useEffect(() => {
+        const search = async () => {
+            const {data} = await axios.get("https://en.wikipedia.org/w/api.php",{
+                params : {
+                    action: 'query',
+                    list: 'search',
+                    format: 'json',
+                    origin: '*',
+                    srsearch: term
+                }
+            });
+
+            console.log(data);
+        }
+
+        search();
+
+    },[term]);
 
     return (
         <div>
             <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term</label> 
-                    <input className="input" />
+                    <input className="input" value={term} onChange={(event) => setTerm(event.target.value)} />
                 </div>
             </div>
         </div>
