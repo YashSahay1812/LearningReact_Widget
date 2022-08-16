64ce84a95377f9674b87cd04622871fd1e34d2c6
unknown <yashsahay999@gmail.com>
2022-08-16 10:12:35 +0530

    Storing the active index inside state

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({items}) => {
 
-    const renderedItems = items.map(({title,content}) => 
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    }
+
+    const renderedItems = items.map(({title,content}, index) => 
         <React.Fragment key={title}>
-            <div className="title active">
+            <div className="title active" onClick={() => onTitleClick(index)}>
                 <i className="dropdown icon"></i>
                 {title}
             </div>
