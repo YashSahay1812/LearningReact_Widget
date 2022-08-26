59197596dd77537d9ce38fdd5546446f2514004c
unknown <yashsahay999@gmail.com>
2022-08-26 09:14:48 +0530

    Added Header through hyperlink

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,20 @@ import React from "react";
 const Header = () => {
 
     return (
-        <>Header</>
+        <div className="ui secondary pointing menu">
+            <a href="/" className="item">
+                Accordion
+            </a>
+            <a href="/list" className="item">
+                List
+            </a>
+            <a href="/dropdown" className="item">
+                Dropdown
+            </a>
+            <a href="/translate" className="item">
+                Translate
+            </a>
+        </div>
     );
 
 }
